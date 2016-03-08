ed84cf055ef3f02de4ecbb4e983fb0daf00fff4a
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-07 19:38:27 -0800

    servers are working

--- src/server.py
+++ src/server.py
@@ -1,17 +1,15 @@
 # coding=utf-8
 import socket
-# import pdb
 
 
 def server():
-    # pdb.set_trace()
-    server = socket.socket(socket.AF_INET,
-                           socket.SOCK_STREAM,
-                           socket.IPPROTO_TCP)
+    server_socket = socket.socket(socket.AF_INET,
+                                  socket.SOCK_STREAM,
+                                  socket.IPPROTO_TCP)
     address = ('127.0.0.1', 5000)
-    server.bind(address)
-    server.listen(1)
-    conn, addr = server.accept()
+    server_socket.bind(address)
+    server_socket.listen(1)
+    conn, addr = server_socket.accept()
 
     buffer_length = 4096
     message_complete = False
@@ -21,10 +19,13 @@ def server():
         print(part.decode('utf-8'))
         if len(part) < buffer_length:
             message_complete = True
-            server.close()
+            server_socket.close()
         return_message += part.decode('utf-8')
     print(return_message)
     conn.sendall(return_message.encode('utf-8'))
     conn.close()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(address)
+    server()
 
 server()
