3c896022ca26d3168fab9cf9006b723eac5f6121
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-08 15:11:46 -0800

    server is working

--- src/server.py
+++ src/server.py
@@ -3,9 +3,11 @@ import socket
 
 
 def server():
+    """Run Server"""
     server_socket = socket.socket(socket.AF_INET,
                                   socket.SOCK_STREAM,
                                   socket.IPPROTO_TCP)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     address = ('127.0.0.1', 5000)
     server_socket.bind(address)
     server_socket.listen(1)
@@ -16,16 +18,16 @@ def server():
     return_message = ''
     while not message_complete:
         part = conn.recv(buffer_length)
-        print(part.decode('utf-8'))
+        # print(part.decode('utf-8'))
         if len(part) < buffer_length:
             message_complete = True
             server_socket.close()
         return_message += part.decode('utf-8')
-    print(return_message)
+    # print(return_message)
     conn.sendall(return_message.encode('utf-8'))
     conn.close()
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(address)
+    # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # server_socket.bind(address)
     server()
 
 server()
