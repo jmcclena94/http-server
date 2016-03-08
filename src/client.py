136ff5afeca8c5585ba0e6a94e88a062cde17aa7
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-07 19:05:45 -0800

    server and client are working

--- src/client.py
+++ src/client.py
@@ -1,16 +1,24 @@
 # coding=utf-8
 import socket
 import sys
-import server
 
 
 def client(message):
-    server()
-    client_socket = socket.getaddrinfo('127.0.0.1', 6000)
+    client_socket = socket.getaddrinfo('127.0.0.1', 5000)
     stream_info = [i for i in client_socket if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
     client.sendall(message.encode('utf-8'))
+
+    buffer_length = 4096
+    reply_complete = False
+    while not reply_complete:
+        part = client.recv(buffer_length)
+        print(part.decode('utf-8'))
+        if len(part) < buffer_length:
+            reply_complete = True
+
+    client.close()
 
 
 if __name__ == "__main__":
--- src/server.py
+++ src/server.py
@@ -1,12 +1,14 @@
 # coding=utf-8
 import socket
+# import pdb
 
 
 def server():
+    # pdb.set_trace()
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 6000)
+    address = ('127.0.0.1', 5000)
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
@@ -16,9 +18,13 @@ def server():
     return_message = ''
     while not message_complete:
         part = conn.recv(buffer_length)
-        return_message = return_message + part
         print(part.decode('utf-8'))
         if len(part) < buffer_length:
             message_complete = True
-
+            server.close()
+        return_message += part.decode('utf-8')
+    print(return_message)
     conn.sendall(return_message.encode('utf-8'))
+    conn.close()
+
+server()
