a72fb3824ed002e70960119bc7113a82557b4cac
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-09 18:27:52 -0800

    tests pass.

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ CF 401 Python http-server Assignment
 
 Collaborators: Joseph McClenahan, Jared Scarr
 
+This app allows for a client to send an http request.
+The server will then parse that request and return either
+a 200 OK response or a 500 Error.
+
+Resourcess:
+
+Python Docs
+stackoverflow.com
+google.com
--- src/test_servers.py
+++ src/test_servers.py
@@ -1,56 +1,39 @@
 # coding=utf-8
 import pytest
 
-# MESSAGE_TABLE = [
-#     ('Hello World', 'Hello World'),
-#     ('', ''),
-#     ('This message is longer than multiple buffer lengths', 'This message is l'
-#      'onger than multiple buffer lengths'),
-#     ('11111111', '11111111'),
-#     ('111111', '111111'),
-#     ('£', '£')
-# ]
 
-RESPONSES = [
-    ('GET', '200 OK'),
-    ('HTTP/1.1', '500'),
-    ('NOT THERE', '404 Not Found'),
+GOOD = [
+    ('GET somepath HTTP/1.1 Host: www.somuchfun.com', 'somepath'),
+]
+
+OK_RESPONSE = [
+    ('fart', 'HTTP/1.1 200 OK\r\n\r\n'),
+    ('Hello World', 'HTTP/1.1 200 OK\r\n\r\n'),
+    ('11111111', 'HTTP/1.1 200 OK\r\n\r\n'),
 ]
 
 
-# OK_RESPONSE = [
-#     ('fart', 'HTTP/1.1 200 OK\r\n\r\n'),
-#     ('Hello World', 'HTTP/1.1 200 OK\r\n\r\n'),
-#     ('11111111', 'HTTP/1.1 200 OK\r\n\r\n'),
-# ]
-
-@pytest.mark.parametrize('request, response', RESPONSES)
-def test_parse_request(request, response):
-    """Test if parse request sends the right errors."""
-    from server import parse_request
-    assert parse_request(request) == response
-
-
-# def test_parse_request():
+# @pytest.mark.parametrize('request, response', GOOD)
+# def test_parse_request_success(request, response):
 #     """Test if parse request sends the right errors."""
 #     from server import parse_request
-#     with pytest.raises(NotImplementedError) as request
-    
 #     assert parse_request(request) == response
 
 
+def test_parse_request_success():
+    """Test if parse request sends the right errors."""
+    from server import parse_request
+    assert parse_request('GET somepath HTTP/1.1 Host: www.somuchfun.com') == 'somepath'
+
+
+def test_parse_request():
+    """Test if parse request sends the right errors."""
+    from server import parse_request
+    with pytest.raises(NotImplementedError):
+        parse_request('GET somechickens HTTP/1.0 Host: www.thisiswrong.com')
+
 
 def test_response_ok():
+    """Test if the response is ok."""
     from server import response_ok
     assert response_ok() == 'HTTP/1.1 200 OK\r\n\r\n'
-
-# @pytest.mark.parametrize('message, reply', MESSAGE_TABLE)
-# def test_client(message, reply):
-#     from client import client
-#     assert client(message) == reply
-
-
-# @pytest.mark.parametrize('request, response', OK_RESPONSE)
-# def test_response_ok(request, response):
-#     from client import client
-#     assert client(request) == response
