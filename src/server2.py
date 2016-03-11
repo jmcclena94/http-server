ec596310dc557b9eb3f3fd31d779318859e8195a
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-11 15:54:11 -0800

    gevent server2 is working

---
+++ src/server2.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+from server import response_ok, response_error, parse_request, resolve_uri
+
+
+def echo(socket, address):
+    """Run Server."""
+    # server_socket = socket.socket(socket.AF_INET,
+    #                               socket.SOCK_STREAM,
+    #                               socket.IPPROTO_TCP)
+    # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # address = ('127.0.0.1', 5000)
+    # server_socket.bind(address)
+    # socket.listen(1)
+    # conn, addr = socket.accept()
+
+    buffer_length = 8
+    # while True:
+    #     while message_complete
+    #     part = socket.recv(buffer_length)
+    #     if len(part) < buffer_length:
+    #         message_complete = True
+    #     return_message += part.decode('utf-8')
+    message_complete = False
+    return_message = ''
+    while not message_complete:
+        # part = conn.recv(buffer_length)
+        part = socket.recv(buffer_length)
+        if len(part) < buffer_length:
+            message_complete = True
+            # socket.close()
+        return_message += part.decode('utf-8')
+    print('Return Message: ' + return_message)
+    try:
+        uri = parse_request(return_message)
+        content = resolve_uri(uri)
+        reply_ok = response_ok()
+        content_type = b'Content Type: ' + content[1] + b'\r\n'
+        content_length = str(len(content[0])).encode('utf-8')
+        content_length = b'Content-Length: ' + content_length + b'\r\n\r\n'
+        reply = reply_ok + content_type + content_length + content[0]
+        print(reply)
+    except NotImplementedError:
+        reply = response_error()
+    except OSError:
+        reply = response_error()
+    socket.sendall(reply)
+    socket.close()
+    # server_socket.close()
+    # server()
+
+
+if __name__ == '__main__':
+    from gevent.server import StreamServer
+    from gevent.monkey import patch_all
+    patch_all()
+    server = StreamServer(('127.0.0.1', 10000), echo)
+    print('Starting echo server on port 10000')
+    server.serve_forever()
